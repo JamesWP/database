51642494f5ecdca35202bddbfac64e2b9b6b8fa7
James Peach <james_peach01@hotmail.co.uk>
2023-03-21 23:19:18 +0000

    added pager allocation and tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod pager;
+
+mod btree {
+    use crate::pager;
+
+    pub struct BTree {
+        pager: pager::Pager,
+    }
+}
+
+mod database {
+    use crate::btree;
+
+    struct Database {
+        btree: btree::BTree,
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/pager.rs
@@ -0,0 +1,271 @@
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Seek, Write, BufReader},
+    os::unix::prelude::MetadataExt,
+    path::Path,
+};
+
+use serde::{Serialize, Deserialize};
+
+pub struct Page {
+    // TODO: maybe share an existing open page
+    content: [u8; PAGE_SIZE as usize],
+}
+
+impl Default for Page {
+    fn default() -> Self {
+        Self { content: [0;PAGE_SIZE as usize] }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ZeroPage {
+    // Contains metadata usefull to the pager
+
+    // TODO: make this the head of a linked list to ensure it is a fixed size when encoding ZeroPage
+    free_page_list: Vec<u32>
+}
+
+impl From<&Page> for ZeroPage {
+    fn from(value: &Page) -> Self {
+        let reader = BufReader::new(value.content.as_slice());
+        let mut deserializer = serde_json::Deserializer::from_reader(reader);
+        ZeroPage::deserialize(&mut deserializer).unwrap()
+    }
+}
+
+pub struct Pager {
+    path: String,
+}
+
+const PAGE_SIZE: u32 = 2 << 11;
+
+impl Pager {
+    pub fn new(path: &str) -> Pager {
+        Pager {
+            path: path.to_owned(),
+        }
+    }
+
+    pub fn get_file_size_pages(&self) -> u32 {
+        let path = Path::new(&self.path);
+        let file = OpenOptions::new()
+            .read(true)
+            .write(false)
+            .open(path)
+            .unwrap();
+
+        file.metadata().unwrap().size() as u32 / PAGE_SIZE
+    }
+
+    pub fn set_file_size_pages(&self, num_pages: u32) {
+        let path = Path::new(&self.path);
+        let file = OpenOptions::new()
+            .read(false)
+            .write(true)
+            .open(path)
+            .unwrap();
+
+        file.set_len(PAGE_SIZE as u64 *num_pages as u64).unwrap();
+    }
+
+    fn get_zero_page(&self) -> ZeroPage {
+        let page = self.get(0);
+        ZeroPage::from(&page)
+    }
+
+    fn set_zero_page(&mut self, zero: ZeroPage) {
+
+        let mut zero_page = Page::default();
+        serde_json::to_writer(zero_page.content.as_mut_slice(), &zero).unwrap();
+        
+        self.set(0, &zero_page);
+    }
+
+    fn file_at_page_readonly(&self, idx: u32) -> File {
+        let path = Path::new(&self.path);
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(false)
+            .open(path)
+            .unwrap();
+        file.seek(std::io::SeekFrom::Start((PAGE_SIZE * idx) as u64))
+            .unwrap();
+
+        file
+    }
+
+    fn file_at_page_write(&mut self, idx: u32) -> File {
+        let path = Path::new(&self.path);
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .open(path)
+            .unwrap();
+        file.seek(std::io::SeekFrom::Start((PAGE_SIZE * idx) as u64))
+            .unwrap();
+
+        file
+    }
+
+    pub fn get(&self, idx: u32) -> Page {
+        let mut p = Page::default();
+
+        let content = p.content.as_mut_slice();
+
+        let mut file = self.file_at_page_readonly(idx);
+        file.read_exact(content).unwrap();
+
+        p
+    }
+
+    pub fn set(&mut self, idx: u32, page: &Page) {
+        let mut file = self.file_at_page_write(idx);
+        file.write_all(&page.content).unwrap(); 
+    }
+
+    pub fn allocate(&mut self) -> u32 {
+        let num_pages = self.get_file_size_pages();
+
+        // we dont have any pages
+        if num_pages == 0 {
+            // Allocate two pages, one for the pager and one to return to the caller
+            self.set_file_size_pages(2);
+
+            // Write out new zero page
+            let zero = ZeroPage{ free_page_list: Default::default() };
+            self.set_zero_page(zero);
+            // New page is the first page
+            1
+        } else {
+            // We need to find the page allocation table in the first page and get a page from its free list
+            let page = self.get(0);
+            let mut zero = ZeroPage::from(&page);
+
+            let page_no = zero.free_page_list.pop();
+
+            self.set_zero_page(zero);
+
+            if let Some(page_no) = page_no {
+                page_no
+            } else {
+                // If there are no pages in the free list we need to expand the filesize
+                // TODO: For performance reasons, maybe increment number of pages by more than one?
+                self.set_file_size_pages(num_pages + 1);
+
+                num_pages
+            }
+        }
+    }
+
+    pub fn dealocate(&mut self, idx: u32) {
+        if idx == 0 {
+            panic!("Cant dealloc page zero");
+        }
+
+        let mut zero = self.get_zero_page();
+
+        if zero.free_page_list.contains(&idx) {
+            panic!("Free list already contains this page!");
+        }
+
+        zero.free_page_list.push(idx);
+
+        self.set_zero_page(zero);
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use tempfile::NamedTempFile;
+
+    use super::Pager;
+
+    #[test]
+    fn simple() {
+        let file = NamedTempFile::new().unwrap();
+        let path = file.path().to_str().unwrap();
+
+        let mut pager = Pager::new(path);
+
+        assert_eq!(0, pager.get_file_size_pages());
+
+        let page_one_idx = pager.allocate();
+
+        let page_two_idx = pager.allocate();
+
+        assert_eq!(3, pager.get_file_size_pages());
+
+        let mut page_one_content = pager.get(page_one_idx);
+        let mut page_two_content = pager.get(page_two_idx);
+
+        page_one_content.content[0] = 10;
+        page_one_content.content[10] = 10;
+
+        page_two_content.content[0] = 20;
+        page_two_content.content[20] = 20;
+
+        pager.set(page_one_idx, &page_one_content);
+        pager.set(page_two_idx, &page_two_content);
+
+        page_one_content.content[0] = 0;
+        page_one_content.content[10] = 0;
+
+        page_two_content.content[0] = 0;
+        page_two_content.content[20] = 0;
+
+        // Re open file from disk
+        let pager = Pager::new(path);
+
+        assert_eq!(3, pager.get_file_size_pages());
+
+        let page_one_content = pager.get(page_one_idx);
+        let page_two_content = pager.get(page_two_idx);
+
+        assert_eq!(10, page_one_content.content[0]);
+        assert_eq!(10, page_one_content.content[10]);
+
+        assert_eq!(20, page_two_content.content[0]);
+        assert_eq!(20, page_two_content.content[20]);
+    }
+
+    #[test]
+    fn free_list() {
+        let file = NamedTempFile::new().unwrap();
+        let path = file.path().to_str().unwrap();
+
+        let mut pager = Pager::new(path);
+
+        let a = pager.allocate();
+        let b = pager.allocate();
+        let c = pager.allocate();
+        let d = pager.allocate();
+        let e = pager.allocate();
+        let f = pager.allocate();
+
+        let max_size = pager.get_file_size_pages();
+
+        pager.dealocate(a);
+        pager.dealocate(c);
+        pager.dealocate(e);
+        pager.dealocate(f);
+        
+        // no shrinking of underlying file
+        assert_eq!(max_size, pager.get_file_size_pages());
+
+        let a2 = pager.allocate();
+        let c2 = pager.allocate();
+        let e2 = pager.allocate();
+        let f2 = pager.allocate();
+
+        // no further allocation needed, dealocated pages reused
+        assert_eq!(max_size, pager.get_file_size_pages());
+
+        // allocate one more page 
+        let g = pager.allocate();
+
+        // more pages allocated
+        assert_eq!(max_size+1, pager.get_file_size_pages());
+    }
+}
