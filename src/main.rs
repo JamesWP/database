10bd8dbada1ce5381ea06a59e1deafe37f865e43
James Peach <james_peach01@hotmail.co.uk>
2023-03-26 18:39:56 +0100

    comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod pager;
 mod node;
+
+/// Btree module heavily inspired by the fantastic article: https://cglab.ca/~abeinges/blah/rust-btree-case/
+/// 
+/// And the btree structures described in: https://www.sqlite.org/fileformat.html
 mod btree;
 
 mod database {
