b57630b7c7661e2604e58d6d9bbe30424bff3ea2
James Peach <james_peach01@hotmail.co.uk>
2023-04-22 13:09:03 +0100

    test split

--- src/node.rs
+++ src/node.rs
@@ -8,8 +8,11 @@ pub enum NodePage<K, V> {
     Interior(InteriorNodePage<K>),
 }
 
-impl<K, V> NodePage<K, V> 
-where K: Ord, K: Clone, V: Clone
+impl<K, V> NodePage<K, V>
+where
+    K: Ord,
+    K: Clone,
+    V: Clone,
 {
     pub fn search(&self, k: &K) -> SearchResult {
         match self {
@@ -78,12 +81,12 @@ where
         // Simple linear search through the page.
         for (index, (key, _value)) in self.cells.iter().enumerate() {
             match search_key.cmp(key) {
-                Less => { return SearchResult::NotPresent(index)},
-                Equal => { return SearchResult::Found(index) },
-                Greater => {}, // Continue the search
+                Less => return SearchResult::NotPresent(index),
+                Equal => return SearchResult::Found(index),
+                Greater => {} // Continue the search
             }
         }
-        
+
         SearchResult::NotPresent(self.cells.len())
     }
 
@@ -91,12 +94,7 @@ where
         self.cells[index] = (key, value);
     }
 
-    pub fn insert_item_at_index(
-        &mut self,
-        index: usize,
-        key: K,
-        value: V,
-    ) {
+    pub fn insert_item_at_index(&mut self, index: usize, key: K, value: V) {
         // put item into leaf at given index.
 
         self.cells.insert(index, (key, value));
@@ -106,43 +104,48 @@ where
         self.cells.get(entry_index)
     }
 
-    pub fn num_items(&self) -> usize{
+    pub fn num_items(&self) -> usize {
         self.cells.len()
     }
 
     pub fn verify_key_ordering(&self) -> Result<(), VerifyError> {
-        let keys = || self.cells.iter().map(|(k,_v)| k);
+        let keys = || self.cells.iter().map(|(k, _v)| k);
 
         for (left, right) in keys().zip(keys()) {
             match left.cmp(right) {
-                Less | Equal => { /* GOOD! */},
+                Less | Equal => { /* GOOD! */ }
                 Greater => {
                     return Err(VerifyError::KeyOutOfOrder);
-                },
+                }
             }
         }
 
         Ok(())
     }
 
-    fn split(&self) -> (LeafNodePage<K, V>, LeafNodePage<K, V>) where K: Clone, V: Clone {
+    fn split(&self) -> (LeafNodePage<K, V>, LeafNodePage<K, V>)
+    where
+        K: Clone,
+        V: Clone,
+    {
         let midpoint = self.cells.len() / 2;
         let (left, right) = self.cells.split_at(midpoint);
 
         let left = Self {
-            cells: left.to_vec()
+            cells: left.to_vec(),
         };
 
         let right = Self {
-            cells: right.to_vec()
+            cells: right.to_vec(),
         };
 
         (left, right)
     }
 }
 
+#[derive(Debug)]
 pub enum VerifyError {
-    KeyOutOfOrder
+    KeyOutOfOrder,
 }
 #[derive(Serialize, Deserialize)]
 pub struct InteriorNodePage<K> {
@@ -151,9 +154,10 @@ pub struct InteriorNodePage<K> {
     // TODO: create interior node
 }
 
-
 #[cfg(test)]
 mod test {
+    use std::collections::HashSet;
+
     use super::{LeafNodePage, SearchResult};
 
     #[test]
@@ -167,7 +171,7 @@ mod test {
         // [1, 2]
         page.insert_item_at_index(2, 3, 0);
         // [1, 2, 3]
-    
+
         assert_eq!(page.cells[0].0, 1);
         assert_eq!(page.cells[1].0, 2);
         assert_eq!(page.cells[2].0, 3);
@@ -193,11 +197,53 @@ mod test {
         assert_eq!(0, found_index(page.search(&1)));
         assert_eq!(1, found_index(page.search(&2)));
         assert_eq!(2, found_index(page.search(&3)));
-    
     }
 
-    #[test] 
-    fn test_split() {
-        todo!();
+    use proptest::{prelude::*};
+
+    proptest! {
+        #[test]
+        fn test_split(insertions in prop::collection::vec(&(0..100, 0..1000),0..100usize)) {
+            let mut page = LeafNodePage::default();
+
+            // Count num unique keys
+            let n = insertions.iter().map(|(k,_v)|k).collect::<HashSet<_>>().len();
+
+            for (key, value) in insertions {
+                let result = page.search(&key);
+                match result {
+                    SearchResult::Found(idx) => {
+                        page.set_item_at_index(idx, key, value);
+                    }
+                    SearchResult::NotPresent(idx) => {
+                        page.insert_item_at_index(idx, key, value);
+                    }
+                    SearchResult::GoDown(_) => panic!(),
+                };
+
+                page.verify_key_ordering().unwrap();
+            }
+
+            // Page has N elements, one for each unique key
+            assert_eq!(n, page.num_items());
+
+            // println!("both {page:?}");
+
+            let (left, right) = page.split();
+
+            // println!("left {left:?} <-> right {right:?}");
+
+            // No items were lost in the making of these parts
+            assert_eq!(left.num_items() + right.num_items(), n);
+
+            let delta = left.num_items().abs_diff(right.num_items());
+
+            assert!(delta == 0 || delta == 1);
+
+            // If we have items in both parts, they should be in order
+            if left.num_items()>0 && right.num_items()>0 {
+                assert!(left.get_item_at_index(left.num_items()-1).unwrap().0 < right.get_item_at_index(0).unwrap().0);
+            }
+        }
     }
 }
